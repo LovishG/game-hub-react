d3f537dd1070b912d050956204c56f50cf6935e4
LovishG <lovish0320.cse19@chitkara.edu.in>
2023-07-17 15:03:10 +0530

    installed chakra UI and created nav and

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        main
+      </GridItem>
+    </Grid>
+  );
+}
+export default App;
