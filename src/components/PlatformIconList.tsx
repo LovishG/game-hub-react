3c47545b1dedd4a4e2ff515f7d9db566a6ee0fab
LovishG <lovish0320.cse19@chitkara.edu.in>
2023-07-18 20:18:19 +0530

    Displaying platform icons and installed react icons library.

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,43 @@
+import { HStack, Icon } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    android: FaAndroid,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY={1}>
+      //1 is a multiple of theme.space value in chakra UI instead we can also
+      use '10px' ...etc instead of 1
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="grey.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
