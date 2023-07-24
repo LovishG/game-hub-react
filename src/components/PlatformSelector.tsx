9c1d4f5406c31d5172ed295ce92dce92c68dfbfa
LovishG <lovish0320.cse19@chitkara.edu.in>
2023-07-24 13:56:29 +0530

    used react query to fetch platforms

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -21,7 +21,7 @@ export const PlatformSelector = ({
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             onClick={() => onSelectPlatform(platform)}
             key={platform.id}
